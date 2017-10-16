e53d3b230610e73769b0bc3dc0b9e760106c3dd1
mateuszkopacz <m.kopacz199@gmail.com>
2017-10-16 22:49:20 +0200

    Scan Card with CardIO

---
+++ ScanCard/ScanCard-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  ScanCard-Bridging-Header.h
+//  ScanCard
+//
+//  Created by Mateusz Kopacz on 15.10.2017.
+//  Copyright © 2017 Mateusz Kopacz. All rights reserved.
+//
+
+#ifndef ScanCard_Bridging_Header_h
+#define ScanCard_Bridging_Header_h
+
+#include <CardIO/CardIO.h>
+
+#endif /* ScanCard_Bridging_Header_h */
--- ScanCard/ViewController.swift
+++ ScanCard/ViewController.swift
@@ -8,7 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CardIOPaymentViewControllerDelegate {
+    
+    @IBOutlet weak var cardNumberLabel: UILabel!
+    
+    @IBOutlet weak var cardDateNumber: UILabel!
+    
+    @IBOutlet weak var CardCCVLabel: UILabel!
+    
+    
+    @IBAction func scanButton(_ sender: UIButton) {
+        
+        let cardIOVC = CardIOPaymentViewController(paymentDelegate: self)
+        cardIOVC?.modalPresentationStyle = .formSheet
+        present(cardIOVC!, animated: true, completion: nil)
+        
+        
+    }
+    func userDidCancel(_ paymentViewController: CardIOPaymentViewController!) {
+        
+        print("User Canceled")
+        paymentViewController.dismiss(animated: true, completion: nil)
+    }
+    
+    func userDidProvide(_ cardInfo: CardIOCreditCardInfo!, in paymentViewController: CardIOPaymentViewController!) {
+        
+        if let info = cardInfo {
+            
+            let cardNumber = info.cardNumber
+            let cardDate = "\(info.expiryMonth)/\(info.expiryYear)"
+            let cardCVV = info.cvv
+            
+            paymentViewController.dismiss(animated: true, completion: nil)
+            
+            cardNumberLabel.text = "Card Number: \(cardNumber!)"
+            cardDateNumber.text = "Exp Date: \(cardDate)"
+            CardCCVLabel.text = "CCV: \(cardCVV!) "
+            
+            
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +58,11 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        CardIOUtilities.preload()
     }
 
 
